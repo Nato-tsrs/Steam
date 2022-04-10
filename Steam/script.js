03b6e3454b28182bd14ceb8d5a3035e7bee5375d
Nato Tsertsvadze <76736484+Nato-tsrs@users.noreply.github.com>
2022-04-10 23:46:46 +0400

    Add files via upload

---
+++ Steam/script.js
@@ -0,0 +1,16 @@
+
+var list=document.getElementById("list");
+function closeList(){
+
+    list.style.visibility="hidden";
+}
+function openList(){
+    list.style.visibility="visible";
+}
+// window.onclick=function(event){
+   
+// if(event.target==list){
+//     alert("dsf");
+// list.style.visibility="hidden";
+// }
+// }
